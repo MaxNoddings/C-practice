3dde9275a6d3fe9e1b6a5510142489601ccde441
Max Noddings <mnodd4@gmail.com>
2024-05-20 21:51:53 -0600

    function introduction

---
+++ functionIntro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1 = -5;
+  int num2 = 42;
+  // Write your code below
+  // printf();
+  printf("Gladiator is an awesome movie.\n");
+  printf("The value of num1 = %i\n", num1);
+  printf("The value of num1 = %i\nThe value of num2 = %i\n", num1, num2);
+}
