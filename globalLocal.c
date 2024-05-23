8204339783941af6d318d7431d13f62f377e05ff
Max Noddings <mnodd4@gmail.com>
2024-05-22 23:28:49 -0600

    Global and Local varibles

---
+++ globalLocal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Write your code below...
+
+char* myMessage = "This is my global message!";
+
+void myFunc() {
+  char* myMessage = "This is my local message!";
+  printf("%s\n", myMessage);
+}
+
+int main() {
+  // You can change `myNumber` to be larger than `50`
+  int myNumber = 665;
+
+  if (myNumber <= 50) {
+    printf("%d\n%s\n", myNumber, myMessage);
+  } else {
+    int myNumber = 500;
+    printf("%d\n", myNumber);
+    myFunc();
+  }
+}
